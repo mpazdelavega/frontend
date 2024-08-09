eb55115656cc9ae184ce8a8b63bca8c34f9cab9f
mpazdelavega <72899415+mpazdelavega@users.noreply.github.com>
2024-08-09 00:01:15 -0400

    Se genera template "Login"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CardComponent } from './component/card/card.component';
 import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
 import { DetailsProductComponent } from './component/details-product/details-product.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'cart',
         component: ShoppingCartComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     },
     {
         path: 'product/:id',
